11b4444cd0b1c38af1fe60cdd3422f4c800e7fe3
Andre Weller <aweller@C-4X42QV2.entw.nemetschek.de>
2019-06-05 15:42:54 +0200

    import store, router and refactor filters

--- src/render/index.js
+++ src/render/index.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import App from '~Components/App'
+import router from './router'
+import {store} from './store'
 
 Vue.config.devtools = true
 Vue.config.productionTip = false
 
-new Vue({
+Vue.filter('calcTime', (depTime) => {
+  let now = Date.now();
+  let diff = depTime - now;
+  depTime = Math.floor(diff/1000/60);
+  return depTime;
+})
+
+Vue.filter('formatTime', (time)=>{
+  const dateFromTime = new Date(time);
+  let formatedDate = dateFromTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
+  return formatedDate;
+})
+
+
+const app = new Vue({
+  router,
+  store,
   el: '#app',
-  render (h) {
-    return h(App)
-  }
+  render: h => h(App),
 })
